8a66c61d291093a9c517af856ee4ea8c0047c0dd
Mark Brocato <mark.brocato@sencha.com>
2017-08-24 11:36:46 -0400

    refactored server out to a separate package

--- packages/apollo-example/server/index.js
+++ packages/apollo-example/server/index.js
@@ -1,32 +1,7 @@
-import webpack from 'webpack';
+import server from 'server';
 import path from 'path';
-import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import bodyParser from 'body-parser';
-import schema from './schema';
-import data from './data';
 
-const port = 8080;
-const app = express();
 const config = require('../webpack.config')();
+const contentBase = path.join(__dirname, '..', 'build');
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ 
-  schema,
-  printErrors: true
-}));
-
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.use(express.static(path.join(__dirname, '..', 'build')));
-
-if (process.env.NODE_ENV !== 'production') {
-    const webpackMiddleware = require("webpack-dev-middleware");
-    app.use(webpackMiddleware(webpack(config)));
-}
-
-app.listen(port, () => console.log(
-  `GraphiQL is now running on http://localhost:${port}/graphiql`
-));
-
-app.get('/employees', (req, res) => {
-  res.json(data)
-})
+server(config, contentBase);
---
+++ packages/server/index.js
@@ -0,0 +1,34 @@
+import webpack from 'webpack';
+import path from 'path';
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import bodyParser from 'body-parser';
+import schema from './schema';
+import data from './data';
+
+export default function(webpackConfig, contentBase) {
+  const port = 8080;
+  const app = express();
+
+  app.use('/graphql', bodyParser.json(), graphqlExpress({ 
+    schema,
+    printErrors: true
+  }));
+
+  app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+  app.use(express.static(contentBase));
+
+  if (process.env.NODE_ENV !== 'production') {
+      const webpackMiddleware = require("webpack-dev-middleware");
+      app.use(webpackMiddleware(webpack(webpackConfig)));
+  }
+
+  app.listen(port, () => console.log(
+    `GraphiQL is now running on http://localhost:${port}/graphiql`
+  ));
+
+  app.get('/employees', (req, res) => {
+    res.json(data)
+  })
+}
+
